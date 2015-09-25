2798e29fee8134a74794cb093fca31ead525d353
Tin Vo <tinvo@Tins-MacBook-Pro.local>
2015-09-25 01:31:06 -0500

    allows security, added UIImageView, and displayed img on the phone. TODO: add prev and next buttons, and maybe resize the UIImangeView to make it look better; cache the image (optional)

--- Comic/ViewController.swift
+++ Comic/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /* An NSURL object represents a URL that can potentially contain the location of a
+        resource on a remote server, the path of a local file on disk, or even an arbitrary
+        piece of encoded data */
+        let url = NSURL(string: "http://imgs.xkcd.com/comics/birthday.png")
+        
+        /* An NSURLRequest object represents a URL load request in a
+        manner independent of protocol and URL scheme */
+        let urlReq = NSURLRequest(URL: url!)
+        
+        /* An NSURLConnection object lets you load the contents
+        of a URL by providing a URL request object */
+        NSURLConnection.sendAsynchronousRequest(urlReq, queue: NSOperationQueue.mainQueue()) { (res, data, err) -> Void in
+            if (err != nil) {
+                print(err)
+            } else {
+                if let xkcdImg = UIImage(data: data!) {
+                    self.img.image = xkcdImg
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
